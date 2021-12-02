6ba591c0f2ab56f550472082b02c03e44a7afdd1
blakeedwards823 <blakeedwards823@gmail.com>
2021-12-01 22:17:36 -0800

    working towards experiment 1, added config files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 import numpy as np
 import time
@@ -7,51 +8,55 @@ import random
 from cell import Cell
 from food import Food
 
-frame_rate = 1
-screen_size = 250
-num_foods = 4
-num_cells = 1
-cell_vision_distance = 50
-timesteps = 0
-generation = 0
-cell_fitness_reproduction_level = 2 # fitness must be this much for cell to clone itself
 
-# randomly init cell
-# let cell run until death
-# while true
-    # if best_cell is none, set best cell to this cell
-    # else if the cell fitness greater than best cell, set best cell to the cell
-cells = []
-best_cells = {}
+class SimulationConfig:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
-foods = []
-time_steps = 0
-
-window = Tk()
-window.title("Fish Tank")
-window.geometry(f"{screen_size}x{screen_size}")
-window.resizable(False, False)
-canvas = Canvas(window, width=screen_size, height=screen_size)
-canvas.pack()
-
-# init foods
-foods.append(Food(canvas, 50, 50))
-foods.append(Food(canvas, screen_size-50, 50))
-foods.append(Food(canvas, 50, screen_size-50))
-foods.append(Food(canvas, screen_size-50, screen_size-50))
-
-# create cells 
-if len(cells) < num_cells:
-    for i in range(0, num_cells-len(cells)):
-        x,y = int(screen_size/2), int(screen_size/2)
-        new_cell = Cell(canvas, x, y, id=i)
-        cells.append(new_cell)
-
-def generate_cell_coordinate(r):
+def generate_blob_coordinate(config, r):
     '''
     Generates a coordinate pair within the window
     '''
-    return randint(0+r, screen_size-r), randint(0+r, screen_size-r)
+    return randint(0+r, config.screen_size-r), randint(0+r, config.screen_size-r)
+
+f = open("config.json", "r")
+data = json.load(f)
+config = SimulationConfig(**data)
+
+time_steps = 0
+generation = 0
+cells = []
+best_cells = {}
+best_fitness = {}
+foods = []
+
+window = Tk()
+window.title("Fish Tank")
+window.geometry(f"{config.screen_size}x{config.screen_size}")
+window.resizable(False, False)
+canvas = Canvas(window, width=config.screen_size, height=config.screen_size)
+canvas.pack()
+
+# init foods
+if config.food_init_pattern == "corners":
+    foods.append(Food(canvas, config.cell_vision_distance, config.cell_vision_distance))
+    foods.append(Food(canvas, config.screen_size-config.cell_vision_distance, config.cell_vision_distance))
+    foods.append(Food(canvas, config.cell_vision_distance, config.screen_size-config.cell_vision_distance))
+    foods.append(Food(canvas, config.screen_size-config.cell_vision_distance, config.screen_size-config.cell_vision_distance))
+elif config.food_init_pattern == "random":
+    for i in range(config.n_foods):
+        x, y = generate_blob_coordinate(config, 10)
+        foods.append(Food(canvas, x, y))
+
+# init cells 
+if config.cell_init_pattern == "center":
+    for i in range(0, config.n_cells):
+        x,y = int(config.screen_size/2), int(config.screen_size/2)
+        cells.append(Cell(canvas, x, y, id=i))
+elif config.cell_init_pattern == "random":
+    for i in range(0, config.n_cells):
+        x, y = generate_blob_coordinate(config, 10)
+        cells.append(Cell(canvas, x, y, id=i))
 
 def generate_hex_color():
     r, g, b = hex(randint(0,255)), hex(randint(0,255)), hex(randint(0,255))
@@ -100,46 +105,66 @@ def evolve_species(eol_cell, best_cells):
     return best_cells
 
 def move():
+    global config
     global cells
     global foods
     global epoch
     global time_steps
     global frame_rate
     global best_cells
+    global best_fitness
     global generation
 
     # if no cells left, time to restart with a new generation
     if len(cells) == 0:
         generation += 1
+        if generation >= config.n_generations:
+            print(f"completed {generation} generations")
+            window.destroy()
+            return
         print(f"GENERATION {generation}")
-        for species in best_cells.keys():
-            print(f"Species {species} highest fitness: {best_cells[species].fitness}")
-            # create new cell with same: id, color, neural netw
-            x, y = int(screen_size/2), int(screen_size/2)
-            parentCell = best_cells[species]
-            childCell = Cell(canvas, x, y, parent_cell=parentCell)
-            childCell.generation = parentCell.generation + 1
-            # initialize with parent network
-            childCell.set_nn_weights(parentCell.get_nn_weights())
-            # add slight mutation to child weights
-            mutation_stddev = random.uniform(0.01, 1.0)
-            childCell.mutate_weights(mutation_stddev)
-            print(f"current child mutation stddev: {mutation_stddev}")
-            # TODO make sure mutation does not modify parents weights because pass by reference
-            childCell.clear_brain_memory() # delete recurrent memory from parent
-            cells.append(childCell)
+        for key in best_cells.keys():
+            print(f"cell {key} best: {best_cells[key].fitness}")
+        if config.cell_inheritance == "random":
+            for i in range(config.n_cells):
+                if config.cell_init_pattern == "center":
+                    x, y = int(config.screen_size/2), int(config.screen_size/2)
+                elif config.cell_init_pattern == "random":
+                    x, y = generate_blob_coordinate(config, 10)
+                cells.append(Cell(canvas, x, y, id=i))
+        elif config.cell_inheritance == "simple":
+            for species in best_cells.keys():
+                print(f"Species {species} highest fitness: {best_cells[species].fitness}")
+                # create new cell with same: id, color, neural netw
+                x, y = int(config.screen_size/2), int(config.screen_size/2)
+                parentCell = best_cells[species]
+                childCell = Cell(canvas, x, y, parent_cell=parentCell)
+                childCell.generation = parentCell.generation + 1
+                # initialize with parent network
+                childCell.set_nn_weights(parentCell.get_nn_weights())
+                # add slight mutation to child weights
+                mutation_stddev = random.uniform(0.01, 1.0)
+                childCell.mutate_weights(mutation_stddev)
+                print(f"current child mutation stddev: {mutation_stddev}")
+                # TODO make sure mutation does not modify parents weights because pass by reference
+                childCell.clear_brain_memory() # delete recurrent memory from parent
+                cells.append(childCell)
         
         # clear and regen foods
         for food in foods:
             food.self_destruct(canvas)
             foods.remove(food)
-        # for i in range(len(foods)):
-        #     x,y = generate_cell_coordinate(40)
-        #     foods.append(Food(canvas, x, y))
-        foods.append(Food(canvas, 50, 50))
-        foods.append(Food(canvas, screen_size-50, 50))
-        foods.append(Food(canvas, 50, screen_size-50))
-        foods.append(Food(canvas, screen_size-50, screen_size-50))
+
+        # init foods
+        if config.food_init_pattern == "corners":
+            foods.append(Food(canvas, config.cell_vision_distance, config.cell_vision_distance))
+            foods.append(Food(canvas, config.screen_size-config.cell_vision_distance, config.cell_vision_distance))
+            foods.append(Food(canvas, config.cell_vision_distance, config.screen_size-config.cell_vision_distance))
+            foods.append(Food(canvas, config.screen_size-config.cell_vision_distance, config.screen_size-config.cell_vision_distance))
+        elif config.food_init_pattern == "random":
+            for i in range(config.n_foods):
+                x, y = generate_blob_coordinate(config, 10)
+                foods.append(Food(canvas, x, y))
         
     # check for collisions between cells and food
     # TODO check cell fitness and spawn new cell if at 
@@ -156,20 +181,6 @@ def move():
         for i in sorted(purge_indexes, reverse=True):
             foods[i].self_destruct(canvas)
             del foods[i]
-    
-    # # TODO create cell clones
-    # for i in clone_indices:
-    #     # x, y = int(screen_size/2), int(screen_size/2)
-    #     x, y = generate_cell_coordinate(100)
-    #     parentCell = cells[i]
-    #     childCell = Cell(canvas, x, y, color=parentCell.color)
-    #     childCell.generation = parentCell.generation + 1
-    #     # initialize with parent 
-    #     childCell.set_nn_weights(parentCell.get_nn_weights())
-    #     # add slight mutation to child weights
-    #     childCell.mutate_weights(0.25)
-    #     childCell.clear_brain_memory() # delete recurrent memory from parent
-    #     cells.append(childCell)
 
     # calculate cell fov vector
     # [N, S, E, W]
@@ -178,13 +189,13 @@ def move():
         fov_walls = [0,0,0,0]
         cell_point = cell.get_coords()
         # calculate if cell sees walls
-        if cell_point[0] < cell_vision_distance:
+        if cell_point[0] < config.cell_vision_distance:
             fov_walls[3] = 1
-        elif cell_point[0] > screen_size - cell_vision_distance:
+        elif cell_point[0] > config.screen_size - config.cell_vision_distance:
             fov_walls[2] = 1
-        if cell_point[1] < cell_vision_distance:
+        if cell_point[1] < config.cell_vision_distance:
             fov_walls[0] = 1
-        elif cell_point[1] > screen_size - cell_vision_distance:
+        elif cell_point[1] > config.screen_size - config.cell_vision_distance:
             fov_walls[1] = 1
         # calculate if cell sees foods
         for food in foods:
@@ -199,12 +210,12 @@ def move():
             v_eye_dist = distance_point_to_line(food_point, v_eye_line)
             v_eye_prox = math.sqrt(math.pow(hypotenuse, 2) - math.pow(v_eye_dist, 2))
 
-            if (h_eye_dist < 10 and h_eye_prox < cell_vision_distance):
+            if (h_eye_dist < 10 and h_eye_prox < config.cell_vision_distance):
                 if cell_point[0] < food_point[0]:
                     fov[2] += 1
                 else:
                     fov[3] += 1
-            if (v_eye_dist < 10 and v_eye_prox < cell_vision_distance):
+            if (v_eye_dist < 10 and v_eye_prox < config.cell_vision_distance):
                 if cell_point[1] > food_point[1]:
                     fov[0] += 1
                 else:
@@ -215,19 +226,19 @@ def move():
 
     # move cells based on fov, only keep cells with fitness above 0 to continue
     for cell in cells:
-        if not cell.advance(canvas, screen_size, screen_size):
-            print(f"Species {cell.id} died, fitness {cell.fitness}")
+        if not cell.advance(canvas, config.screen_size, config.screen_size):
+            # print(f"Species {cell.id} died, fitness {cell.fitness}")
             best_cells = evolve_species(cell, best_cells)
             cells.remove(cell)
 
-    # # create foods if not at minimum foods
-    # if len(foods) < num_foods:
-    #     for i in range(0, num_foods-len(foods)):
-    #         x,y = generate_cell_coordinate(40)
-    #         foods.append(Food(canvas, x, y))
+            # record best cell fitnesses 
+            if cell.id not in best_fitness:
+                best_fitness[cell.id] = [best_cells[cell.id].fitness]
+            else:
+                best_fitness[cell.id].append(best_cells[cell.id].fitness)
 
     time_steps += 1
-    window.after(frame_rate, move)
+    window.after(config.frame_delta, move)
 
 print("Starting movement...")
 move()
